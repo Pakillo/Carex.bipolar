52f25fc7b4ea02191fc224fe949c78f2ff63e8ea
Pakillo <f.rodriguez.sanc@gmail.com>
2017-04-27 18:03:09 +0200

    added bioregions data

--- data-raw/dataprep.R
+++ data-raw/dataprep.R
@@ -1,3 +1,5 @@
+
+## Read occurrence data
 library(readr)
 
 occs <- read_csv("data-raw/monocot_30m.csv")
@@ -7,3 +9,15 @@ occs$species[occs$species == "Carex_magellanica"] <- "magellanica"
 occs$species[occs$species == "Carex_microglochin"] <- "microglochin"
 
 write_csv(occs, "data-raw/monocot_30m.csv")
+
+
+
+## Prepare bioregions
+library(raster)
+andes <- shapefile("data-raw/bioregions/regiones_biogeo_andes3.shp")
+nearctic <- shapefile("data-raw/bioregions/regiones_biogeo_andes2_nearcti.shp")
+regions <- bind(andes, nearctic)
+
+#shapefile(regions, filename = "data-raw/regions.shp")
+save(regions, file = "data/regions.rda")
+
