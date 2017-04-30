095224b9b807460960a1ed567ad1a54eb2e814c9
Pakillo <f.rodriguez.sanc@gmail.com>
2017-04-30 16:03:55 +0200

    added makefile

---
+++ makefile.R
@@ -0,0 +1,30 @@
+source("R/helper.R")   # substitute this by packaging in the end!
+
+
+### Data prep (run only once) ###
+
+#source("data-raw/dataprep.R")
+#source("data-raw/clip_bioregions.R")
+
+############
+
+library(rmarkdown)
+
+
+#### Run Maxent models ####
+crosval <- FALSE
+spp <- c("allspp", "macloviana", "magellanica", "microglochin", "canescens", "maritima")
+for (i in spp) {
+  species <- i
+  render(input = "analyses/models.Rmd",
+         output_file = paste0(species, ".html"))
+}
+
+
+
+
+#### Fig 1: occurrence map ####
+render("manuscript/figures/Fig1_occmap.Rmd")
+
+
+
