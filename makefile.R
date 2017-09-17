286f74425664ba4401cb398c3fb70bf93bdbc1a3
Pakillo <f.rodriguez.sanc@gmail.com>
2017-09-17 23:22:32 +0200

    updated makefile

--- makefile.R
+++ makefile.R
@@ -12,13 +12,25 @@ library(Carex.bipolar)  # load to access package functions
 
 library(rmarkdown)
 
+spp <- c("allspp", "canescens", "macloviana", "magellanica", "maritima",
+         "microglochin")
 
-#### Run Maxent models ####
-crosval <- FALSE
-spp <- c("allspp", "macloviana", "magellanica", "microglochin", "canescens", "maritima")
+
+### Use ENMeval to choose best models ###
+
 for (i in spp) {
   species <- i
-  render(input = "analyses/models.Rmd",
+  render(input = "analyses/fullspp_ENMeval.Rmd",
+         output_dir = file.path("analyses/output/fullspp_ENMeval", species),
+         output_file = paste0(species, ".html"))
+}
+
+
+#### Make predictions for present and future based on best Maxent models ####
+for (i in spp) {
+  species <- i
+  render(input = "analyses/fullspp_predictions.Rmd",
+         output_dir = file.path("analyses/output/fullspp_predictions", species),
          output_file = paste0(species, ".html"))
 }
 
@@ -34,3 +46,8 @@ render("manuscript/figures/Fig_suitability_present_code.Rmd")
 #### Fig Future Suitability ####
 render("manuscript/figures/Fig_suitability_2050_code.Rmd")
 
+
+
+## Calculate SD of predictions for RCP4.5 and RCP8.5 (Suppl Fig)
+
+## Calculate difference Future - Present for both RCP4.5 and RCP8.5 (based on mean) - Suppl Fig
